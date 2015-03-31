65739c5faed9b6a8cbc365d564bf563a4726ac66
Allen Hair <allenhair@google.com>
2015-03-30 18:27:37 -0700

    Rename PlatformWebViewJankTests to UbWebViewJankTests.
    Change-Id: Ibef17469244297cd77cbd57423a6cb4640ebbad6

--- jank/webview/Android.mk
+++ jank/webview/Android.mk
@@ -15,7 +15,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_PACKAGE_NAME := PlatformWebViewJankTests
+LOCAL_PACKAGE_NAME := UbWebViewJankTests
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_MODULE_TAGS := tests
 
