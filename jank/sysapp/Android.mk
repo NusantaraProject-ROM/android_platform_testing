51181949dcd40a2ff7f0293cd1c90bf98653b52a
Md Haque <haque@google.com>
2015-06-11 18:43:13 -0700

    test apk name correction
    Change-Id: I096375e9beee1d55e729d4c4c74ac9b6a44e30bd

--- jank/sysapp/Android.mk
+++ jank/sysapp/Android.mk
@@ -15,7 +15,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_PACKAGE_NAME := AndroidSysAppJankTests
+LOCAL_PACKAGE_NAME := SystemAppJankTests
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_MODULE_TAGS := tests
 
