a38a32c189fd1cd3abc10e7e44121d92a490d1cb
Jed Estep <jestep@google.com>
2016-02-18 12:44:16 -0800

    smokefast: add MediaPlaybackTest and machinery
    Change-Id: I3bac62fd3bde7085c4e42c5aed2409c27f225f22

--- tests/smokefast/Android.mk
+++ tests/smokefast/Android.mk
@@ -5,9 +5,11 @@ LOCAL_MODULE_TAGS := tests
 
 LOCAL_SDK_VERSION := current
 
+media_framework_app_base := frameworks/base/media/tests/MediaFrameworkTest/src/com/android/mediaframeworktest
+
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_JAVA_LIBRARIES := android.test.runner
-LOCAL_STATIC_JAVA_LIBRARIES := ub-uiautomator
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-test ub-uiautomator
 # TODO: AuptLib and aupt-helpers should be static deps as well
 
 LOCAL_PACKAGE_NAME := SmokeFastTests
