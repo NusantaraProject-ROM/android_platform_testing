4ad007cca803d7d52176809deda8e22edfdb8001
Guang Zhu <guangzhu@google.com>
2016-03-27 16:13:39 -0700

    move crash collector into its own folder
    Change-Id: Ia51054e1b806fb7ebbf1aacd739d4b04d116a2ee

--- utils/crashcollector/Android.mk
+++ utils/crashcollector/Android.mk
@@ -18,7 +18,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := crashcollector
-LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/local/tmp
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/local/tmp/crashcollector
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
@@ -28,7 +28,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := crashcollector
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/local/tmp
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)/local/tmp/crashcollector
 LOCAL_SRC_FILES := crashcollector
 
 include $(BUILD_PREBUILT)
