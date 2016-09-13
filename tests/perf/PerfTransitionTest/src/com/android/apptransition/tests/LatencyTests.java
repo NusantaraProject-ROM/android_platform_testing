6ac741e0258a1bab8096486177817278a492df86
Jorim Jaggi <jjaggi@google.com>
2016-09-13 16:28:06 -0700

    Add latency tracking tests for wake-and-unlock and recents
    Change-Id: Ice73a6fa908a35627aa91f03a1728eb5e51931fc

--- tests/perf/PerfTransitionTest/src/com/android/apptransition/tests/LatencyTests.java
+++ tests/perf/PerfTransitionTest/src/com/android/apptransition/tests/LatencyTests.java
@@ -17,13 +17,22 @@ package com.android.apptransition.tests;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.provider.Settings;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.Until;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Tests to test various latencies in the system.
@@ -32,6 +41,10 @@ public class LatencyTests {
 
     private static final int DEFAULT_ITERATION_COUNT = 10;
     private static final String KEY_ITERATION_COUNT = "iteration_count";
+    private static final long CLOCK_SETTLE_DELAY = 2000;
+    private static final String FINGERPRINT_WAKE_FAKE_COMMAND = "am broadcast -a "
+            + "com.android.systemui.ACTION_FINGERPRINT_WAKE_FAKE";
+    private static final String AM_START_COMMAND_TEMPLATE = "am start -a %s";
 
     private UiDevice mDevice;
     private int mIterationCount;
@@ -42,6 +55,7 @@ public class LatencyTests {
         Bundle args = InstrumentationRegistry.getArguments();
         mIterationCount = Integer.parseInt(args.getString(KEY_ITERATION_COUNT,
                 Integer.toString(DEFAULT_ITERATION_COUNT)));
+        mDevice.pressHome();
     }
 
     /**
@@ -58,7 +72,7 @@ public class LatencyTests {
             mDevice.waitForIdle();
 
             // Wait for clocks to settle down
-            SystemClock.sleep(2000);
+            SystemClock.sleep(CLOCK_SETTLE_DELAY);
         }
     }
 
@@ -75,5 +89,85 @@ public class LatencyTests {
                 mDevice.getDisplayWidth() / 2,
                 0,
                 15);
+    }
+
+    /**
+     * Test to track how long it takes until the animation starts in a fingerprint-wake-and-unlock
+     * sequence.
+     * <p>
+     * Every iteration will output a log in the form of "LatencyTracker/action=2 delay=x".
+     */
+    @Test
+    public void testFingerprintWakeAndUnlock() throws Exception {
+        for (int i = 0; i < mIterationCount; i++) {
+            mDevice.sleep();
+
+            // Wait for clocks to settle down
+            SystemClock.sleep(CLOCK_SETTLE_DELAY);
+
+            mDevice.executeShellCommand(FINGERPRINT_WAKE_FAKE_COMMAND);
+            mDevice.waitForIdle();
+        }
+    }
+
+    /**
+     * Test that measure how long the total time takes until recents is visible after pressing it.
+     * Note that this is different from {@link AppTransitionTests#testAppToRecents} as we are
+     * measuring the full latency here, but in the app transition test we only measure the time
+     * spent after startActivity is called. This might be different as SystemUI does a lot of binder
+     * calls before calling startActivity.
+     * <p>
+     * Every iteration will output a log in the form of "LatencyTracker/action=1 delay=x".
+     */
+    @Test
+    public void testAppToRecents() throws Exception {
+        populateRecentApps();
+        for (int i = 0; i < mIterationCount; i++) {
+            mDevice.executeShellCommand(String.format(AM_START_COMMAND_TEMPLATE,
+                    Settings.ACTION_SETTINGS));
+            mDevice.waitForIdle();
+
+            // Wait for clocks to settle.
+            SystemClock.sleep(CLOCK_SETTLE_DELAY);
+            pressUiRecentApps();
+            mDevice.waitForIdle();
+
+            // Make sure all the animations are really done.
+            SystemClock.sleep(200);
+        }
+    }
+
+    private void pressUiRecentApps() throws Exception {
+        mDevice.findObject(By.res("com.android.systemui", "recent_apps")).click();
+    }
+
+    // TODO: Share this code with the jank tests.
+    private void populateRecentApps() throws IOException {
+        PackageManager pm = getInstrumentation().getContext().getPackageManager();
+        List<PackageInfo> packages = pm.getInstalledPackages(0);
+        for (PackageInfo pkg : packages) {
+            if (pkg.packageName.equals(getInstrumentation().getTargetContext().getPackageName())) {
+                continue;
+            }
+            Intent intent = pm.getLaunchIntentForPackage(pkg.packageName);
+            if (intent == null) {
+                continue;
+            }
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            getInstrumentation().getTargetContext().startActivity(intent);
+
+            // Don't overload the system
+            SystemClock.sleep(500);
+        }
+
+        // Give the apps some time to finish starting. Some apps start another activity while
+        // starting, and we don't want to happen when we are testing stuff.
+        SystemClock.sleep(3000);
+
+        // Close any crash dialogs
+        while (mDevice.hasObject(By.textContains("has stopped"))) {
+            mDevice.performActionAndWait(() -> mDevice.pressBack(), Until.newWindow(), 2000);
+        }
     }
 }
