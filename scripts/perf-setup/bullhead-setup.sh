70817607051b2bca4f920bec8c1df085d0e1d4a1
John Reck <jreck@google.com>
2016-10-21 07:50:09 -0700

    Update angler & bullhead scripts
    Change angler & bullhead to lock clocks
    to sustainable levels instead of max levels
    so that they do not hit thermal throttling
    during test suite runs
    
    Test: manual, ran script as root
    Change-Id: I38f0b95a264571c13f9a131e9df184e2e06c5c10

--- scripts/perf-setup/bullhead-setup.sh
+++ scripts/perf-setup/bullhead-setup.sh
@@ -5,17 +5,32 @@ fi
 stop thermal-engine
 stop perfd
 
-echo -n 1 > /sys/devices/system/cpu/cpu4/online
-echo -n performance > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-echo -n 1 > /sys/devices/system/cpu/cpu5/online
-echo -n performance > /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor
+cpubase=/sys/devices/system/cpu
+gov=cpufreq/scaling_governor
 
-echo -n 0 > /sys/devices/system/cpu/cpu0/online
-echo -n 0 > /sys/devices/system/cpu/cpu1/online
-echo -n 0 > /sys/devices/system/cpu/cpu2/online
-echo -n 0 > /sys/devices/system/cpu/cpu3/online
+cpu=0
+S=960000
+while [ $((cpu < 4)) -eq 1 ]; do
+    echo 1 > $cpubase/cpu${cpu}/online
+    echo userspace > $cpubase/cpu${cpu}/$gov
+    echo $S > $cpubase/cpu${cpu}/cpufreq/scaling_max_freq
+    echo $S > $cpubase/cpu${cpu}/cpufreq/scaling_min_freq
+    echo $S > $cpubase/cpu${cpu}/cpufreq/scaling_setspeed
+    cpu=$(($cpu + 1))
+done
 
-echo performance > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+echo -n 0 > /sys/devices/system/cpu/cpu4/online
+echo -n 0 > /sys/devices/system/cpu/cpu5/online
+
 echo 0 > /sys/class/kgsl/kgsl-3d0/bus_split
 echo 1 > /sys/class/kgsl/kgsl-3d0/force_clk_on
 echo 10000 > /sys/class/kgsl/kgsl-3d0/idle_timer
+
+echo 7102 > /sys/class/devfreq/qcom,gpubw.19/min_freq
+
+echo performance > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+echo 300000000 > /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
+echo 300000000 > /sys/class/kgsl/kgsl-3d0/devfreq/max_freq
+
+echo 4 > /sys/class/kgsl/kgsl-3d0/min_pwrlevel
+echo 4 > /sys/class/kgsl/kgsl-3d0/max_pwrlevel
