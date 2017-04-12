3df1365e25aea5377355a4f2b1754c0189369b6f
Hyungtae Tim Kim <hyungtaekim@google.com>
2017-04-12 11:25:46 -0700

    Update TV platform jank test with new launcher
    UI interaction on new launcher is different from
    the legacy launcher.
    - No Settings row
    - The System Row has the Settings button
    
    Bug: 36811015
    Bug: 36857789
    Test: am instrument -w -r -e iterations 1 com.android.androidtv.janktests/android.test.InstrumentationTestRunner
    Change-Id: Ide89e24753aa50ba1b81c89668d046c77615223c

--- tests/jank/androidtvjanktests/Android.mk
+++ tests/jank/androidtvjanktests/Android.mk
@@ -19,7 +19,8 @@ LOCAL_PACKAGE_NAME := AndroidTVJankTests
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_MODULE_TAGS := tests
 
-LOCAL_STATIC_JAVA_LIBRARIES := ub-janktesthelper ub-uiautomator timeresult-helper-lib
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    ub-janktesthelper ub-uiautomator timeresult-helper-lib dpad-util
 
 LOCAL_SDK_VERSION := 21
 
