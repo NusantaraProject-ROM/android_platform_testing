2fb9c05b351a885cd9aae6e089be0a92b328e3c9
Michael Rosenfeld <mrosenfeld@google.com>
2017-05-05 12:30:55 -0700

    Add a QSB interface to the App Helper Library.
    * Adds an init method in the HelperTest for better 1-time dismissal.
    * Create sub-methods in HelperTest for flexibility.
    
    Bug: 31159285
    Test: in linked CL
    Change-Id: Id397a34bc9451aefc33ce2f8f68e6d0b2ee3bbde

--- libraries/app-helpers/common/src/android/platform/test/helpers/common/test/HelperTest.java
+++ libraries/app-helpers/common/src/android/platform/test/helpers/common/test/HelperTest.java
@@ -16,12 +16,14 @@
 
 package android.platform.test.helpers.common.test;
 
+import android.os.Bundle;
 import android.platform.test.helpers.HelperManager;
 import android.platform.test.helpers.AbstractStandardAppHelper;
 import android.platform.test.helpers.listeners.FailureTestWatcher;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.uiautomator.UiDevice;
 import android.system.helpers.PackageHelper;
+import android.util.Log;
 
 import org.junit.After;
 import org.junit.Before;
@@ -30,6 +32,8 @@ import org.junit.rules.TestRule;
 import org.junit.rules.Timeout;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.android.permissionutils.GrantPermissionUtil;
 
@@ -39,6 +43,12 @@ import com.android.permissionutils.GrantPermissionUtil;
  * @param T the helper interface under test.
  */
 public abstract class HelperTest<T extends AbstractStandardAppHelper> {
+    private static final String LOG_TAG = HelperTest.class.getSimpleName();
+    private static final String SKIP_INIT_PARAM = "skip-init";
+
+    // Keep track (across tests) of the initialized applications.
+    private static Map<Class, Boolean> mInitMap = new HashMap<Class, Boolean>();
+
     // Global 5-minute test timeout
     @Rule
     public final TestRule timeout = Timeout.millis(Duration.ofMinutes(5).toMillis());
@@ -56,18 +66,46 @@ public abstract class HelperTest<T extends AbstractStandardAppHelper> {
     protected T mHelper;
 
     @Before
-    public void setUp() {
+    public <T> void setUp() {
+        if (!mInitMap.containsKey(getHelperClass()) &&
+                !"true".equals(getArguments().get(SKIP_INIT_PARAM))) {
+            initialize();
+            mInitMap.put(getHelperClass(), true);
+        }
+        resetApp();
+        openApp();
+    }
+
+    public void initialize() { }
+
+    @After
+    public void tearDown() {
+        exitApp();
+    }
+
+    /**
+     * Reset the target application by clearing user data and re-granting runtime permissions.
+     */
+    public void resetApp() {
         // Clear the application's state.
         PackageHelper.getInstance(InstrumentationRegistry.getInstrumentation())
                 .cleanPackage(getHelper().getPackage());
         // Re-grant cleared runtime permissions.
         GrantPermissionUtil.grantAllPermissions(InstrumentationRegistry.getContext());
+    }
+
+    /**
+     * Open the target application.
+     */
+    public void openApp() {
         // Open the application.
         getHelper().open();
     }
 
-    @After
-    public void tearDown() {
+    /**
+     * Exit the target application.
+     */
+    public void exitApp() {
         // Exit the application.
         getHelper().exit();
     }
@@ -75,7 +113,7 @@ public abstract class HelperTest<T extends AbstractStandardAppHelper> {
     /**
      * @return the connected @{code UiDevice}
      */
-    protected UiDevice getDevice() {
+    public UiDevice getDevice() {
         if (mDevice == null) {
             mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
         }
@@ -84,9 +122,16 @@ public abstract class HelperTest<T extends AbstractStandardAppHelper> {
     }
 
     /**
+     * @return the @{code Bundle} of args
+     */
+    public Bundle getArguments() {
+        return InstrumentationRegistry.getArguments();
+    }
+
+    /**
      * @return an implementation for {@code T}
      */
-    protected T getHelper() {
+    public T getHelper() {
         if (mHelper == null) {
             mHelper = HelperManager.getInstance(
                     InstrumentationRegistry.getContext(),
