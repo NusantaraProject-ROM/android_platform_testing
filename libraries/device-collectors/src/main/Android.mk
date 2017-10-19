f74842b530ff654634df7d6054bf5bc185b0c983
jdesprez <jdesprez@google.com>
2017-10-19 12:31:25 -0700

    Set up the device collector project for presubmit
    Do the configuration and proper setup to be ready to run
    the tests in presubmit to validate the library.
    
    Test: unit tests
    make general-tests
    ./tradefed run template/local_min --template:map
    test=collector-device-lib-test
    Bug: 36351803
    
    Change-Id: Ibb2ae653c05d105c54443285b65e51e8b7fffa6d

--- libraries/device-collectors/src/main/Android.mk
+++ libraries/device-collectors/src/main/Android.mk
@@ -16,7 +16,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src/main)
+LOCAL_SRC_FILES := $(call all-java-files-under, java)
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-test \
