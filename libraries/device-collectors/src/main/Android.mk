2155f50e4ee627b904b4a7a38337b2d96ae1fb3b
Julien Desprez <jdesprez@google.com>
2017-11-11 09:38:51 +0000

    Revert "Remove android-support-test deps"
    This reverts commit 9d77a73b9a2ece41efbe3500eca2e18a4399f152.
    
    Change-Id: Iadccf19f8ccf491208917d77b02b793f056a893f

--- libraries/device-collectors/src/main/Android.mk
+++ libraries/device-collectors/src/main/Android.mk
@@ -18,7 +18,9 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, java)
 
-LOCAL_STATIC_JAVA_LIBRARIES := junit android-support-test-nodep android-support-annotations
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-test \
+    junit
 
 LOCAL_MODULE_TAGS := optional
 
