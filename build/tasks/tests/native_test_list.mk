972e01d8f4c3f92b220a69b55df4906e79eafb34
Lalit Maganti <lalitm@google.com>
2018-02-07 19:19:44 +0000

    testing: enable perfetto tests to be built as part of APCT package
    Test: N/A
    Change-Id: I7a6a80864888d6505758fab1d6475584d6d9b339

--- build/tasks/tests/native_test_list.mk
+++ build/tasks/tests/native_test_list.mk
@@ -81,6 +81,8 @@ native_tests := \
     netd_unit_test \
     nfc_test_utils \
     pagemap_test \
+    perfetto_unittests \
+    perfetto_integrationtests \
     perfprofd_test \
     posix_async_io_test \
     recovery_component_test \
