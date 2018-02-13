eeb6fcc59107b04ead4f35510d2bdd527c8bac83
gopinath <gelanchezhian@google.com>
2018-02-12 16:46:31 -0800

    Fix build errors.
    Bug: b/73254069
    
    Change-Id: I351cc85385921e6e6ca99dfc61f08f97afdfd76d

--- tests/functional/devicehealthtests/src/com/android/devicehealth/tests/KnownFailures.java
+++ tests/functional/devicehealthtests/src/com/android/devicehealth/tests/KnownFailures.java
@@ -32,8 +32,8 @@ public class KnownFailures {
         // add known failures per dropbox label below:
         addKnownFailure(
                 "system_app_anr",  // dropbox label
-                Pattern.compile("^.*Process:.*com\\.google\\.android\\
-                        .googlequicksearchbox:search.*$", Pattern.MULTILINE),  // regex pattern
+                Pattern.compile("^.*Process:.*com\\.google\\.android\\.googlequicksearchbox"
+                        + ":search.*$", Pattern.MULTILINE),  // regex pattern
                 "73254069");  // bug number
         // add known failures for additional dropbox labels here
         // It's recommended to use multiline matching and a snippet will be matched if it's found
