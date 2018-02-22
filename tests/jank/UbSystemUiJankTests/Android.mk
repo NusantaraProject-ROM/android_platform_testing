33b07f9c6ab977dfb7e4ba1658730394655857d1
Anton Hansson <hansson@google.com>
2018-02-22 17:42:11 +0000

    platform_testing: Set LOCAL_SDK_VERSION where possible.
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Test: make relevant packages
    Bug: 73535841
    Change-Id: I64d4cc11fa869346763cd960e7d336ae2975669d
    Merged-In: I28f983577ded466ee9714c8155674ceaca373fcb

--- tests/jank/UbSystemUiJankTests/Android.mk
+++ tests/jank/UbSystemUiJankTests/Android.mk
@@ -16,6 +16,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_PACKAGE_NAME := UbSystemUiJankTests
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_MODULE_TAGS := tests
 
