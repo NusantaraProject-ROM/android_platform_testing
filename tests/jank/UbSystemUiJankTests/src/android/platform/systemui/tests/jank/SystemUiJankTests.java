8a920e53880e00dec7276da60e7573bc76c7db8a
Lucas Dupin <dupin@google.com>
2018-03-15 11:10:55 -0700

    Lower min frame count of testPinAppearance
    Bug: 74602128
    Test: runtest systemui-jank -c android.platform.systemui.tests.jank.SystemUiJankTests -m testPinAppearance
    Change-Id: I39cb2a70c258ad2d9573062ef517e9e55fcbd4ea

--- tests/jank/UbSystemUiJankTests/src/android/platform/systemui/tests/jank/SystemUiJankTests.java
+++ tests/jank/UbSystemUiJankTests/src/android/platform/systemui/tests/jank/SystemUiJankTests.java
@@ -888,9 +888,9 @@ public class SystemUiJankTests extends JankTestBase {
     }
 
     /**
-     * Measures jank when launching the camera from lockscreen.
+     * Measures jank when swiping up and showing pin on lockscreen.
      */
-    @JankTest(expectedFrames = 30,
+    @JankTest(expectedFrames = 20,
             defaultIterationCount = 5,
             beforeTest = "beforePinAppearance",
             afterTest = "afterPinAppearance",
