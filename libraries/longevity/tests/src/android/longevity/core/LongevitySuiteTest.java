d19df606070043f7482f620ac592a9ab265f092a
Michael Rosenfeld <mrosenfeld@google.com>
2018-04-20 15:29:05 -0700

    Add a child accounting test to the longevity suite tests.
    Bug: 78360655
    Test: ran host-side tests as prescribed by longevity/README.md
    Change-Id: Id0f02ebdf186f20887743581d57aae6adc568b01

--- libraries/longevity/tests/src/android/longevity/core/LongevitySuiteTest.java
+++ libraries/longevity/tests/src/android/longevity/core/LongevitySuiteTest.java
@@ -15,7 +15,13 @@
  */
 package android.longevity.core;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+
+import android.longevity.core.scheduler.Iterate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.internal.builders.AllDefaultPossibilitiesBuilder;
@@ -29,6 +35,8 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(JUnit4.class)
 public class LongevitySuiteTest {
+    private static final int TEST_ITERATIONS = 10000;
+
     /**
      * Unit test that the {@link SuiteClasses} annotation is required.
      */
@@ -44,4 +52,42 @@ public class LongevitySuiteTest {
 
     @RunWith(LongevitySuite.class)
     private static class NoSuiteClassesSuite { }
+
+    /**
+     *
+     */
+    @Test
+    public void testChildAccounting() throws InitializationError {
+        Map<String, String> args = new HashMap();
+        args.put(Iterate.OPTION_NAME, String.valueOf(TEST_ITERATIONS));
+        LongevitySuite suite =
+                new LongevitySuite(TestSuite.class, new AllDefaultPossibilitiesBuilder(true), args);
+        assertEquals(suite.testCount(), TEST_ITERATIONS * 3);
+    }
+
+
+    @RunWith(LongevitySuite.class)
+    @SuiteClasses({
+        TestSuite.OneTest.class,
+        TestSuite.TwoTests.class
+    })
+    /**
+     * Sample device-side test cases.
+     */
+    public static class TestSuite {
+        // no local test cases.
+
+        public static class OneTest {
+            @Test
+            public void testNothing1() { }
+        }
+
+        public static class TwoTests {
+            @Test
+            public void testNothing1() { }
+
+            @Test
+            public void testNothing2() { }
+        }
+    }
 }
