e74e77ddcb1e55a186a88abfe113db7165e0143a
gopinath <gelanchezhian@google.com>
2018-09-17 09:39:39 -0700

    Add CpuUsageListener and helpers.
    Bug: b/111928925, b/114808437
    
    Listener output : https://paste.googleplex.com/6420737422262272
    
    Test: atest
    CollectorshelperTest:com.android.helpers.tests.CpuUsageHelperTest -
    Pass
    
    Change-Id: I20d05b4a65051a702e2af0e1db0a553e31af99e9

--- libraries/collectors-helper/src/com/android/helpers/StatsdHelper.java
+++ libraries/collectors-helper/src/com/android/helpers/StatsdHelper.java
@@ -19,28 +19,39 @@ package com.android.helpers;
 import android.content.Context;
 import android.app.StatsManager;
 import android.app.StatsManager.StatsUnavailableException;
+import android.os.SystemClock;
 import android.util.Log;
+import android.util.StatsLog;
 import androidx.test.InstrumentationRegistry;
 
 import com.android.internal.os.StatsdConfigProto.AtomMatcher;
 import com.android.internal.os.StatsdConfigProto.EventMetric;
+import com.android.internal.os.StatsdConfigProto.FieldFilter;
+import com.android.internal.os.StatsdConfigProto.GaugeMetric;
 import com.android.internal.os.StatsdConfigProto.SimpleAtomMatcher;
 import com.android.internal.os.StatsdConfigProto.StatsdConfig;
+import com.android.internal.os.StatsdConfigProto.TimeUnit;
+import com.android.os.AtomsProto.Atom;
 import com.android.os.StatsLog.ConfigMetricsReport;
 import com.android.os.StatsLog.ConfigMetricsReportList;
 import com.android.os.StatsLog.EventMetricData;
+import com.android.os.StatsLog.GaugeMetricData;
 import com.android.os.StatsLog.StatsLogReport;
+
+import com.google.protobuf.InvalidProtocolBufferException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * StatsdHelper consist of basic utilities that will be used to setup statsd
  * config, parse the collected information and remove the statsd config.
  */
 public class StatsdHelper {
-
     private static final String LOG_TAG = StatsdHelper.class.getSimpleName();
+    private static final long MAX_ATOMS = 2000;
+    private static final long METRIC_DELAY_MS = 3000;
     private long mConfigId = -1;
     private StatsManager mStatsManager;
 
@@ -52,32 +63,87 @@ public class StatsdHelper {
      */
     public boolean addEventConfig(int atomId) {
         long configId = System.currentTimeMillis();
-        String atomName = "Atom" + System.currentTimeMillis();
-        String eventName = "Event" + System.currentTimeMillis();
+        int atomUniqueId = getUniqueId();
         StatsdConfig config = getSimpleSources(configId)
                 .addEventMetric(
                         EventMetric.newBuilder()
-                                .setId(eventName.hashCode())
-                                .setWhat(atomName.hashCode())
+                                .setId(getUniqueId())
+                                .setWhat(atomUniqueId)
                 )
-                .addAtomMatcher(
-                        AtomMatcher.newBuilder()
-                                .setId(atomName.hashCode())
-                                .setSimpleAtomMatcher(
-                                        SimpleAtomMatcher.newBuilder()
-                                                .setAtomId(atomId)
-                                )
-                )
+                .addAtomMatcher(getSimpleAtomMatcher(atomUniqueId, atomId))
                 .build();
         try {
             getStatsManager().addConfig(configId, config.toByteArray());
         } catch (Exception e) {
-            Log.e(LOG_TAG, "Not able to setup the config due to :", e);
+            Log.e(LOG_TAG, "Not able to setup the event config.", e);
             return false;
         }
         Log.i(LOG_TAG, "Successfully added config with config-id:" + configId);
         setConfigId(configId);
         return true;
+    }
+
+    /**
+     * Build gauge metric config based on trigger events (i.e AppBreadCrumbReported).
+     * Whenever the events are triggered via StatsLog.logEvent() collect the gauge metrics.
+     * It doesn't matter what the log event is. It could be 0 or 1.
+     * In order to capture the usage during the test take the difference of gauge metrics
+     * before and after the test.
+     *
+     * @param atomIdList List of atoms to be collected in gauge metrics.
+     * @return if the config is added successfully otherwise false.
+     */
+    public boolean addGaugeConfig(List<Integer> atomIdList) {
+        long configId = System.currentTimeMillis();
+        StatsdConfig.Builder statsConfigBuilder = getSimpleSources(configId);
+        int appBreadCrumbUniqueId = getUniqueId();
+
+        // Needed for collecting gauge metric based on trigger events.
+        statsConfigBuilder.addAtomMatcher(getSimpleAtomMatcher(appBreadCrumbUniqueId,
+                Atom.APP_BREADCRUMB_REPORTED_FIELD_NUMBER));
+
+        for (Integer atomId : atomIdList) {
+            int atomUniqueId = getUniqueId();
+            // Build Gauge metric config.
+            GaugeMetric.Builder gaugeMetric = GaugeMetric.newBuilder()
+                    .setId(getUniqueId())
+                    .setWhat(atomUniqueId)
+                    .setGaugeFieldsFilter(FieldFilter.newBuilder().setIncludeAll(true).build())
+                    .setMaxNumGaugeAtomsPerBucket(MAX_ATOMS)
+                    .setSamplingType(GaugeMetric.SamplingType.ALL_CONDITION_CHANGES)
+                    .setTriggerEvent(appBreadCrumbUniqueId)
+                    .setBucket(TimeUnit.CTS);
+
+            // add the gauge config.
+            statsConfigBuilder.addAtomMatcher(getSimpleAtomMatcher(atomUniqueId, atomId))
+                    .addGaugeMetric(gaugeMetric.build());
+        }
+
+        try {
+            getStatsManager().addConfig(configId,
+                    statsConfigBuilder.build().toByteArray());
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Not able to setup the gauge config.", e);
+            return false;
+        }
+
+        Log.i(LOG_TAG, "Successfully added config with config-id:" + configId);
+        setConfigId(configId);
+        return true;
+    }
+
+    /**
+     * Create simple atom matcher with the given id and the field id.
+     *
+     * @param id
+     * @param fieldId
+     * @return
+     */
+    private AtomMatcher.Builder getSimpleAtomMatcher(int id, int fieldId) {
+        return AtomMatcher.newBuilder()
+                .setId(id)
+                .setSimpleAtomMatcher(SimpleAtomMatcher.newBuilder()
+                        .setAtomId(fieldId));
     }
 
     /**
@@ -108,8 +174,8 @@ public class StatsdHelper {
                 reportList = ConfigMetricsReportList.parser()
                         .parseFrom(getStatsManager().getReports(getConfigId()));
             }
-        } catch (Exception e) {
-            Log.e(LOG_TAG, "getData failed", e);
+        } catch (InvalidProtocolBufferException | StatsUnavailableException se) {
+            Log.e(LOG_TAG, "Retreiving event metrics failed.", se);
             return eventData;
         }
 
@@ -121,6 +187,35 @@ public class StatsdHelper {
         }
         Log.i(LOG_TAG, "Number of events: " + eventData.size());
         return eventData;
+    }
+
+    /**
+     * Returns the list of GaugeMetric data tracked under the config.
+     */
+    public List<GaugeMetricData> getGaugeMetrics() {
+        // Dump the metric after the test is completed.
+        StatsLog.logEvent(0);
+        SystemClock.sleep(METRIC_DELAY_MS);
+        ConfigMetricsReportList reportList = null;
+        List<GaugeMetricData> gaugeData = new ArrayList<>();
+        try {
+            if (getConfigId() != -1) {
+                reportList = ConfigMetricsReportList.parser()
+                        .parseFrom(getStatsManager().getReports(getConfigId()));
+            }
+        } catch (InvalidProtocolBufferException | StatsUnavailableException se) {
+            Log.e(LOG_TAG, "Retreiving gauge metrics failed.", se);
+            return gaugeData;
+        }
+
+        if (reportList != null) {
+            ConfigMetricsReport configReport = reportList.getReports(0);
+            for (StatsLogReport metric : configReport.getMetricsList()) {
+                gaugeData.addAll(metric.getGaugeMetrics().getDataList());
+            }
+        }
+        Log.i(LOG_TAG, "Number of Gauge data: " + gaugeData.size());
+        return gaugeData;
     }
 
     /**
@@ -155,6 +250,16 @@ public class StatsdHelper {
     }
 
     /**
+     * Returns the package name for the UID if it is available. Otherwise return null.
+     *
+     * @param uid
+     * @return
+     */
+    public String getPackageName(int uid) {
+        return InstrumentationRegistry.getTargetContext().getPackageManager().getNameForUid(uid);
+    }
+
+    /**
      * Set the config id tracked in the statsd.
      */
     private void setConfigId(long configId) {
@@ -166,5 +271,12 @@ public class StatsdHelper {
      */
     private long getConfigId() {
         return mConfigId;
+    }
+
+    /**
+     * Returns the unique id
+     */
+    private int getUniqueId() {
+        return UUID.randomUUID().hashCode();
     }
 }
