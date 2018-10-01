f018b813bfc079f5a9903c008cc2834e0677fe79
Harry Zhang <harrytczhang@google.com>
2018-10-01 11:26:51 -0700

    Added total usage to CpuUsageHelper.
    Bug: 116804007
    Test: CpuUsageHelperTest.java
    Change-Id: I2bbb3c6be4380fab2bf53400ec277e4fd690758d

--- libraries/collectors-helper/test/src/com/android/helpers/tests/CpuUsageHelperTest.java
+++ libraries/collectors-helper/test/src/com/android/helpers/tests/CpuUsageHelperTest.java
@@ -28,6 +28,7 @@ import org.junit.runner.RunWith;
 import java.util.Map;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Android Unit tests for {@link CpuUsageHelperTest}.
@@ -44,6 +45,10 @@ public class CpuUsageHelperTest {
     private static final String CPU_USAGE_FREQ_PREFIX = "cpu_usage_freq";
     // Key prefix used for cpu usage by package name or uid
     private static final String CPU_USAGE_PKG_UID_PREFIX = "cpu_usage_pkg_or_uid";
+    // Key used for total CPU usage
+    private static final String TOTAL_CPU_USAGE = "total_cpu_usage";
+    // Key used for total CPU usage in frequency buckets
+    private static final String TOTAL_CPU_USAGE_FREQ = "total_cpu_usage_freq";
 
     private CpuUsageHelper mCpuUsageHelper = new CpuUsageHelper();
     private HelperAccessor<ICalendarHelper> mHelper =
@@ -77,32 +82,64 @@ public class CpuUsageHelperTest {
     }
 
     /**
-     * Test atleast one cpu usage per pkg or uid and per preq index is collected.
+     * Test that at least one cpu usage per pkg or uid and per preq index is collected,
+     * the total usage is collected, and that
+     * the total usage is indeed the sum of the per pkg/uid and frequency usage, respectively.
      */
     @Test
     public void testCpuUsageMetricsKey() throws Exception {
+        // Variables to verify existence of collected metrics.
         boolean isFreqIndexPresent = false;
         boolean isPkgorUidPresent = false;
         boolean isFreqUsed = false;
         boolean isUIDUsed = false;
+        boolean isTotalCpuUsageEntryPresent = false;
+        boolean isTotalCpuUsageValuePresent = false;
+        boolean isTotalCpuFreqEntryPresent = false;
+        boolean isTotalCpuFreqValuePresent = false;
         assertTrue(mCpuUsageHelper.startCollecting());
         mHelper.get().open();
+        // Variables to Verify that the reported usage does sum up to the reported total usage.
+        Long sumCpuUsage = 0L;
+        Long sumCpuUsageFreq = 0L;
+        Long reportedTotalCpuUsage = 0L;
+        Long reportedTotalCpuUsageFreq = 0L;
         for (Map.Entry<String, Long> cpuUsageEntry : mCpuUsageHelper.getMetrics().entrySet()) {
-            if (cpuUsageEntry.getKey().contains(CPU_USAGE_FREQ_PREFIX)) {
+            if (cpuUsageEntry.getKey().startsWith(CPU_USAGE_FREQ_PREFIX)) {
                 isFreqIndexPresent = true;
                 if (cpuUsageEntry.getValue() > 0) {
                     isFreqUsed = true;
                 }
+                sumCpuUsageFreq += cpuUsageEntry.getValue();
             }
-            if (cpuUsageEntry.getKey().contains(CPU_USAGE_PKG_UID_PREFIX)) {
+            if (cpuUsageEntry.getKey().startsWith(CPU_USAGE_PKG_UID_PREFIX)) {
                 isPkgorUidPresent = true;
                 if (cpuUsageEntry.getValue() > 0) {
                     isUIDUsed = true;
                 }
+                sumCpuUsage += cpuUsageEntry.getValue();
+            }
+            if (cpuUsageEntry.getKey().equals(TOTAL_CPU_USAGE_FREQ)) {
+                isTotalCpuFreqEntryPresent = true;
+                if (cpuUsageEntry.getValue() > 0) {
+                    isTotalCpuFreqValuePresent = true;
+                }
+                reportedTotalCpuUsageFreq = cpuUsageEntry.getValue();
+            }
+            if (cpuUsageEntry.getKey().equals(TOTAL_CPU_USAGE)) {
+                isTotalCpuUsageEntryPresent = true;
+                if (cpuUsageEntry.getValue() > 0) {
+                    isTotalCpuUsageValuePresent = true;
+                }
+                reportedTotalCpuUsage = cpuUsageEntry.getValue();
             }
         }
         assertTrue(isFreqIndexPresent && isFreqUsed);
         assertTrue(isPkgorUidPresent && isUIDUsed);
+        assertTrue(isTotalCpuUsageEntryPresent && isTotalCpuUsageValuePresent);
+        assertTrue(isTotalCpuFreqEntryPresent && isTotalCpuFreqValuePresent);
+        assertEquals(sumCpuUsageFreq, reportedTotalCpuUsageFreq);
+        assertEquals(sumCpuUsage, reportedTotalCpuUsage);
         assertTrue(mCpuUsageHelper.stopCollecting());
         mHelper.get().exit();
     }
