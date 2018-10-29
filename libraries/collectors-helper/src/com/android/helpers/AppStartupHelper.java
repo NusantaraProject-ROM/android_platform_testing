88083dba3b4a1b65e4ecdc6756237fb15927168d
gopinath <gelanchezhian@google.com>
2018-10-29 13:48:35 -0700

    Add Statsdhelper and Metric Collectors
    Bug: b/118482428
    Change-Id: Ib93c69fc381e992b5ff1b0b67aa11fe6c062202c
    Merged-In: I5af3d0203329e025a8deb0c2f4bda1f2dbe96b35
    Merged-In: I20d05b4a65051a702e2af0e1db0a553e31af99e9

---
+++ libraries/collectors-helper/src/com/android/helpers/AppStartupHelper.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.helpers;
+
+import android.util.Log;
+
+import com.android.os.AtomsProto.AppStartOccurred;
+import com.android.os.AtomsProto.Atom;
+import com.android.os.StatsLog.EventMetricData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * AppStartupHelper consist of helper methods to set the app
+ * startup configs in statsd to track the app startup related
+ * performance metrics and retrieve the necessary information from
+ * statsd using the config id.
+ */
+public class AppStartupHelper implements ICollectorHelper<StringBuilder> {
+
+    private static final String LOG_TAG = AppStartupHelper.class.getSimpleName();
+    private static final String COLD_STARTUP = "cold_startup";
+    private static final String WARM_STARTUP = "warm_startup";
+    private static final String HOT_STARTUP = "hot_startup";
+
+    private StatsdHelper mStatsdHelper = new StatsdHelper();
+
+    /**
+     * Set up the app startup statsd config to track the metrics during the app start occurred.
+     */
+    @Override
+    public boolean startCollecting() {
+        Log.i(LOG_TAG, "Adding AppStartOccured config to statsd.");
+        List<Integer> atomIdList = new ArrayList<>();
+        atomIdList.add(Atom.APP_START_OCCURRED_FIELD_NUMBER);
+        return mStatsdHelper.addEventConfig(atomIdList);
+    }
+
+    /**
+     * Collect the app startup metrics tracked during the app startup occurred from the statsd.
+     */
+    @Override
+    public Map<String, StringBuilder> getMetrics() {
+        List<EventMetricData> eventMetricData = mStatsdHelper.getEventMetrics();
+        Map<String, StringBuilder> appStartResultMap = new HashMap<>();
+        for (EventMetricData dataItem : eventMetricData) {
+            AppStartOccurred appStartAtom = dataItem.getAtom().getAppStartOccurred();
+            String pkgName = appStartAtom.getPkgName();
+            String transitionType = appStartAtom.getType().toString();
+            int windowsDrawnMillis = appStartAtom.getWindowsDrawnDelayMillis();
+            Log.i(LOG_TAG, String.format("Pkg Name: %s, Transition Type: %s,"
+                    + " WindowDrawnDelayMillis:%s", pkgName, transitionType, windowsDrawnMillis));
+
+            String metricKey = "";
+            switch (appStartAtom.getType()) {
+                case COLD:
+                    metricKey = MetricUtility.constructKey(COLD_STARTUP, appStartAtom.getPkgName());
+                    break;
+                case WARM:
+                    metricKey = MetricUtility.constructKey(WARM_STARTUP, appStartAtom.getPkgName());
+                    break;
+                case HOT:
+                    metricKey = MetricUtility.constructKey(HOT_STARTUP, appStartAtom.getPkgName());
+                    break;
+                case UNKNOWN:
+                    break;
+            }
+            if (!metricKey.isEmpty()) {
+                MetricUtility.addMetric(metricKey, windowsDrawnMillis, appStartResultMap);
+            }
+        }
+        return appStartResultMap;
+    }
+
+    /**
+     * Remove the statsd config used to track the app startup metrics.
+     */
+    @Override
+    public boolean stopCollecting() {
+        return mStatsdHelper.removeStatsConfig();
+    }
+}
