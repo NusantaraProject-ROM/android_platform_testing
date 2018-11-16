36524529381a4a7a256c7839315722ea31f7ef13
Eric Wang <jjunwangg@google.com>
2018-11-15 16:36:58 -0800

    android auto mojave datetime setting test interface
    modified and removed some methods
    
    bug: 119215496
    Test: publicly revealed methods for date and time setting test
    Change-Id: I6ef658130c084a4487ba075d8f5d4deaf3252b3c

---
+++ libraries/app-helpers/interfaces/auto/src/android/platform/helpers/IAutoDateTimeSettingsHelper.java
@@ -0,0 +1,87 @@
+package android.platform.helpers;
+
+/**
+ * Helper class for functional tests of date & time
+ */
+
+import android.support.test.uiautomator.UiObject2;
+import java.util.Date;
+
+public interface IAutoDateTimeSettingsHelper extends IAppHelper {
+    /**
+     * Setup expectation: Date & time setting is open
+     *
+     * This method is to set the device date manually
+     *
+     * @param month - input month
+     * @param day - input day
+     * @param year - input year
+     *
+     */
+    void setDate(int year, int month, int day);
+
+    /**
+     * Setup expectation: Date & time setting is open
+     *
+     * This method is to get the current date displayed on the UI
+     *
+     *
+     */
+    Date getDate();
+
+    /**
+     * Setup expectation: Date & time setting is open
+     *
+     * This method is to set the time manually
+     *
+     * @param hour - input hour
+     * @param minute - input minute
+     * @param AM_PM - input am/pm
+     *
+     */
+    void setTime(int hour, int minute, boolean is_am);
+
+    /**
+     * Setup expectation: Date & time setting is open
+     *
+     * This method is to get the current time displayed on the UI
+     * The return string format will match the UI format exactly
+     *
+     */
+    String getTime();
+
+    /**
+     * Setup expectation: Date & time setting is open
+     *
+     * This method is to set the timezone manually
+     *
+     * @param timezone - city selected for timezone
+     *
+     */
+    void setTimeZone(String timezone);
+
+    /**
+     * Setup expectation: Date & time setting is open
+     *
+     * This method is to get the current time displayed on the UI
+     *
+     */
+    String getTimeZone();
+
+    /**
+     * Setup expectation: Date & time setting is open
+     *
+     * This method is to get the 24 hour format menu switch widget object
+     *
+     */
+    UiObject2 getUseTwentyFourHourFormatSwitchWidget();
+
+    /**
+     * Setup expectation: Date & time setting is open
+     *
+     * This method is to toggle on/off 24 hour format
+     *
+     *
+     */
+    boolean toggleTwentyFourHourFormatSwitch();
+}
