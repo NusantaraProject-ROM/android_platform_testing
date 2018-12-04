12d74ec04619f29dfbc6cb66fa88e269249aad6e
Lucas Dupin <dupin@google.com>
2018-12-03 18:53:24 -0800

    Re-enable AOD on jank tests
    Enable AOD in jank tests and fix issue where
    AmbientDisplayConfiguration constructor was
    being stripped off by proguard
    
    Test: atest google/perf/jank/SystemUI/UbSystemUIJankTests
    Change-Id: Ie4f905b2ea52d7ba2759d4037665a8feb859b5f2
    Fixes: 120088636

--- tests/jank/UbSystemUiJankTests/src/android/platform/systemui/tests/jank/SystemUiJankTests.java
+++ tests/jank/UbSystemUiJankTests/src/android/platform/systemui/tests/jank/SystemUiJankTests.java
@@ -140,12 +140,11 @@ public class SystemUiJankTests extends JankTestBase {
 
         // Enable AOD, otherwise we won't test all animations. Having AOD off also adds
         // unpredictable fluctuations since the display can take up to 200ms to turn on.
-        // TODO: Fix b/120088636 before uncommenting this code.
-        /*AmbientDisplayConfiguration configuration =
+        AmbientDisplayConfiguration configuration =
                 new AmbientDisplayConfiguration(getInstrumentation().getContext());
         mInitialDozeAlwaysOn = configuration.alwaysOnEnabled(UserHandle.USER_SYSTEM) ? 1 : 0;
         ContentResolver contentResolver = getInstrumentation().getContext().getContentResolver();
-        Settings.Secure.putInt(contentResolver, Settings.Secure.DOZE_ALWAYS_ON, 1);*/
+        Settings.Secure.putInt(contentResolver, Settings.Secure.DOZE_ALWAYS_ON, 1);
 
         mLauncher = new LauncherInstrumentation(getInstrumentation());
         mDevice.executeShellCommand("pm disable com.google.android.music");
@@ -161,9 +160,9 @@ public class SystemUiJankTests extends JankTestBase {
         mDevice.executeShellCommand("pm enable com.google.android.music");
         mDevice.unfreezeRotation();
         unblockNotifications();
-        /*ContentResolver contentResolver = getInstrumentation().getContext().getContentResolver();
+        ContentResolver contentResolver = getInstrumentation().getContext().getContentResolver();
         Settings.Secure.putInt(contentResolver, Settings.Secure.DOZE_ALWAYS_ON,
-                mInitialDozeAlwaysOn);*/
+                mInitialDozeAlwaysOn);
         super.tearDown();
     }
 
