020c5beb1c5f0a79b2ff40536a41973ed50f8e9d
Eric Wang <jjunwangg@google.com>
2018-12-12 15:28:14 -0800

    Added method due to change in menu structure
    Languages menu changed to Languages & input menu and Languages sub menu
    was added. New method revealed for test.
    
    Test: this is an interface. Test is done by testing the acutal test.
    bug: 120940944
    
    Change-Id: I608162e74c70819b489bbd3214bfb329c4d0fd75

--- libraries/app-helpers/interfaces/auto/src/android/platform/helpers/IAutoSystemSettingsHelper.java
+++ libraries/app-helpers/interfaces/auto/src/android/platform/helpers/IAutoSystemSettingsHelper.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package android.platform.helpers;
 
 /**
@@ -8,77 +24,75 @@ import java.util.Date;
 
 public interface IAutoSystemSettingsHelper extends IAppHelper {
     /**
-     * Setup expectation: System setting is open
+     * Setup expectation: System setting is open.
      *
-     * This method is to set display language
+     * Set display language.
      *
-     * @param language - input language
-     *
+     * @param language - input language.
      */
     void setDisplayLanguage(String language);
 
     /**
-     * Setup expectation: System setting is open
+     * Setup expectation: System setting is open.
      *
-     * This method is to get current display language
-     *
+     * Get current display language.
      */
     String getCurrentLanguage();
 
     /**
-     * Setup expectation: System setting is open
+     * Setup expectation: System setting is open.
      *
-     * This method is to get device model number from UI
-     *
+     * Get device model number from UI.
      */
     String getDeviceModel();
 
     /**
-     * Setup expectation: System setting is open
+     * Setup expectation: System setting is open.
      *
-     * This method is to get android version from UI
-     *
+     * Get android version from UI.
      */
     String getAndroidVersion();
 
     /**
-     * Setup expectation: System setting is open
+     * Setup expectation: System setting is open.
      *
-     * This method is to get android security patch level from UI
-     *
+     * Get android security patch level from UI.
      */
     Date getAndroidSecurityPatchLevel();
 
     /**
-     * Setup expectation: System setting is open
+     * Setup expectation: System setting is open.
      *
-     * This method is to get kernel version from UI
-     *
+     * Get kernel version from UI.
      */
     String getKernelVersion();
 
     /**
-     * Setup expectation: System setting is open
+     * Setup expectation: System setting is open.
      *
-     * This method is to get build number from UI
-     *
+     * Get build number from UI.
      */
     String getBuildNumber();
 
     /**
-     * Setup expectation: System setting is open
+     * Setup expectation: System setting is open.
      *
-     * This method is to reset network connection. [ Wifi & Bluetooth ]
-     *
+     * Reset network connection [ Wifi & Bluetooth ].
      */
     void resetNetwork();
 
 
     /**
-     * Setup expectation: System setting is open
+     * Setup expectation: System setting is open.
      *
-     * This method is to reset application preferences
-     *
+     * Reset application preferences.
      */
     void resetAppPreferences();
+
+    /**
+     * Setup expectation: System setting is open.
+     *
+     * Open Languages & input menu.
+     */
+    void openLanguagesInputMenu();
 }
