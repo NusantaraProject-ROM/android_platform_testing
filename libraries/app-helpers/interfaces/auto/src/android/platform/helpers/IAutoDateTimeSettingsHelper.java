f070bbdafd59e2edfbee2284437074a6f2eafceb
Jun <jjunwangg@google.com>
2019-03-28 18:07:36 -0700

    change Java doc and method return type
    changed Java doc and method return type from UiObject2 to boolean
    
    Change-Id: Ie658ea4405972980815ddf55ca419cc968500142

--- libraries/app-helpers/interfaces/auto/src/android/platform/helpers/IAutoDateTimeSettingsHelper.java
+++ libraries/app-helpers/interfaces/auto/src/android/platform/helpers/IAutoDateTimeSettingsHelper.java
@@ -3,81 +3,72 @@ package android.platform.helpers;
 /**
  * Helper class for functional tests of date & time
  */
-import android.support.test.uiautomator.UiObject2;
+
 import java.time.LocalDate;
 
 public interface IAutoDateTimeSettingsHelper extends IAppHelper {
     /**
      * Setup expectation: Date & time setting is open
      *
-     * This method is to set the device date manually
+     * Set the device date
      *
      * @param date - input LocalDate object
-     *
      */
     void setDate(LocalDate date);
 
     /**
      * Setup expectation: Date & time setting is open
      *
-     * This method is to get the current date displayed on the UI in LocalDate object
-     *
+     * Get the current date displayed on the UI in LocalDate object
      */
     LocalDate getDate();
 
     /**
      * Setup expectation: Date & time setting is open
      *
-     * This method is to set the time manually
+     * Set the device time
      *
      * @param hour - input hour
      * @param minute - input minute
      * @param AM_PM - input am/pm
-     *
      */
     void setTime(int hour, int minute, boolean is_am);
 
     /**
      * Setup expectation: Date & time setting is open
      *
-     * This method is to get the current time displayed on the UI
+     * Get the current time displayed on the UI
      * The return string format will match the UI format exactly
-     *
      */
     String getTime();
 
     /**
      * Setup expectation: Date & time setting is open
      *
-     * This method is to set the timezone manually
+     * Set the device time zone
      *
      * @param timezone - city selected for timezone
-     *
      */
     void setTimeZone(String timezone);
 
     /**
      * Setup expectation: Date & time setting is open
      *
-     * This method is to get the current time displayed on the UI
-     *
+     * Get the current timezone displayed on the UI
      */
     String getTimeZone();
 
     /**
      * Setup expectation: Date & time setting is open
      *
-     * This method is to get the 24 hour format menu switch widget object
-     *
+     * Check if the 24 hour format menu switch widget is toggoled on
      */
-    UiObject2 getUseTwentyFourHourFormatSwitchWidget();
+    boolean isUseTwentyFourHourFormatSwitchWidgetOn();
 
     /**
      * Setup expectation: Date & time setting is open
      *
-     * This method is to toggle on/off 24 hour format
-     *
-     *
+     * Toggle on/off 24 hour format widget switch
      */
     boolean toggleTwentyFourHourFormatSwitch();
 }
