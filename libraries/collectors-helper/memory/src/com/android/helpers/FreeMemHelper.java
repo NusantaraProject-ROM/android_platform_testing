5a334124f50ae1e1ef2be8b253903102f84a038b
gopinath <gelanchezhian@google.com>
2019-07-23 12:33:50 -0700

    Fix FreeMemListener and TotalPssListener
    Fixes issue in FreeMemListener not parsing the cached proc mem.
    
    Report the metrics in bytes for total pss and mem available cache proc.
    
    Report mem available and cache process meory separately from
    FreeMemListener.
    
    Bug: b/137954774
    
    Test: FreeMemHelperTest and TotalPssHelperTest
    Change-Id: I61fc8e53384e64f383de6f98ec28df270cec3e3b

--- libraries/collectors-helper/memory/src/com/android/helpers/FreeMemHelper.java
+++ libraries/collectors-helper/memory/src/com/android/helpers/FreeMemHelper.java
@@ -21,8 +21,14 @@ import android.util.Log;
 
 import androidx.test.InstrumentationRegistry;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -40,17 +46,18 @@ import java.util.regex.Pattern;
 public final class FreeMemHelper implements ICollectorHelper<Long> {
     private static final String TAG = FreeMemHelper.class.getSimpleName();
     private static final String SEPARATOR = "\\s+";
-    private static final String CACHED_PROCESSES =
-            "dumpsys meminfo|awk '/Total PSS by category:"
-                    + "/{found=0} {if(found) print} /: Cached/{found=1}'|tr -d ' '";
+    private static final String DUMPSYS_MEMIFNO = "dumpsys meminfo";
     private static final String PROC_MEMINFO = "cat /proc/meminfo";
     private static final String LINE_SEPARATOR = "\\n";
     private static final String MEM_AVAILABLE_PATTERN = "^MemAvailable.*";
-    private static final Pattern PID_PATTERN = Pattern.compile("^.*pid(?<processid>[0-9]*).*$");
+    private static final Pattern CACHE_PROC_START_PATTERN = Pattern.compile(".*: Cached$");
+    private static final Pattern PID_PATTERN = Pattern.compile("^.*pid(?<processid> [0-9]*).*$");
     private static final String DUMPSYS_PROCESS = "dumpsys meminfo %s";
     private static final String MEM_TOTAL = "^\\s+TOTAL\\s+.*";
     private static final String PROCESS_ID = "processid";
-    public static final String MEM_AVAILABLE_CACHE_PROC_DIRTY = "MemAvailable_CacheProcDirty_kb";
+    public static final String MEM_AVAILABLE_CACHE_PROC_DIRTY = "MemAvailable_CacheProcDirty_bytes";
+    public static final String PROC_MEMINFO_MEM_AVAILABLE= "proc_meminfo_memavailable_bytes";
+    public static final String DUMPSYS_CACHED_PROC_MEMORY= "dumpsys_cached_procs_memory_bytes";
 
     private UiDevice mUiDevice;
 
@@ -87,25 +94,24 @@ public final class FreeMemHelper implements ICollectorHelper<Long> {
             Log.e(TAG, "MemAvailable is null.");
             return null;
         }
-        long cacheProcDirty = Long.parseLong(memAvailable[1]);
+        Map<String, Long> results = new HashMap<>();
+        long memAvailableProc = Long.parseLong(memAvailable[1]);
+        results.put(PROC_MEMINFO_MEM_AVAILABLE, (memAvailableProc * 1024));
 
-        String cachedProcesses;
-        try {
-            cachedProcesses = mUiDevice.executeShellCommand(CACHED_PROCESSES);
-        } catch (IOException ioe) {
-            Log.e(TAG, "Failed to find cached processes.", ioe);
-            return null;
-        }
+        long cacheProcDirty = memAvailableProc;
+        byte[] dumpsysMemInfoBytes = MetricUtility.executeCommandBlocking(DUMPSYS_MEMIFNO,
+                InstrumentationRegistry.getInstrumentation());
+        List<String> cachedProcList = getCachedProcesses(dumpsysMemInfoBytes);
+        Long cachedProcMemory = 0L;
 
-        String[] processes = cachedProcesses.split(LINE_SEPARATOR);
-
-        for (String process : processes) {
+        for (String process : cachedProcList) {
+            Log.i(TAG, "Cached Process" + process);
             Matcher match;
             if (((match = matches(PID_PATTERN, process))) != null) {
                 String processId = match.group(PROCESS_ID);
                 String processDumpSysMemInfo = String.format(DUMPSYS_PROCESS, processId);
                 String processInfoStr;
-
+                Log.i(TAG, "Process Id of the cached process" + processId);
                 try {
                     processInfoStr = mUiDevice.executeShellCommand(processDumpSysMemInfo);
                 } catch (IOException ioe) {
@@ -124,16 +130,20 @@ public final class FreeMemHelper implements ICollectorHelper<Long> {
                     String[] procDetails = processInfo[0].trim().split(SEPARATOR);
                     int privateDirty = Integer.parseInt(procDetails[2].trim());
                     int privateClean = Integer.parseInt(procDetails[3].trim());
+                    cachedProcMemory = cachedProcMemory + privateDirty + privateClean;
                     cacheProcDirty = cacheProcDirty + privateDirty + privateClean;
-                    Log.d(TAG, "Cached process: " + process + " Private Dirty: "
-                            + privateDirty + " Private Clean: " + privateClean);
+                    Log.i(TAG, "Cached process: " + process + " Private Dirty: "
+                            + (privateDirty * 1024) + " Private Clean: " + (privateClean * 1024));
                 }
             }
         }
 
-        HashMap<String, Long> memAvailableCacheProcDirty = new HashMap<>(1);
-        memAvailableCacheProcDirty.put(MEM_AVAILABLE_CACHE_PROC_DIRTY, cacheProcDirty);
-        return memAvailableCacheProcDirty;
+        // Sum of all the cached process memory.
+        results.put(DUMPSYS_CACHED_PROC_MEMORY, (cachedProcMemory * 1024));
+
+        // Mem available cache proc dirty (memavailable + cachedProcMemory)
+        results.put(MEM_AVAILABLE_CACHE_PROC_DIRTY, (cacheProcDirty * 1024));
+        return results;
     }
 
     /**
@@ -145,5 +155,42 @@ public final class FreeMemHelper implements ICollectorHelper<Long> {
     private static Matcher matches(Pattern pattern, String line) {
         Matcher ret = pattern.matcher(line);
         return ret.matches() ? ret : null;
+    }
+
+    /**
+     * Get cached process information from dumpsys meminfo.
+     *
+     * @param dumpsysMemInfoBytes
+     * @return list of cached processes.
+     */
+    List<String> getCachedProcesses(byte[] dumpsysMemInfoBytes) {
+        List<String> cachedProcessList = new ArrayList<String>();
+        InputStream inputStream = new ByteArrayInputStream(dumpsysMemInfoBytes);
+        boolean isCacheProcSection = false;
+        try (BufferedReader bfReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String currLine = null;
+            while ((currLine = bfReader.readLine()) != null) {
+                Log.i(TAG, currLine);
+                Matcher match;
+                if (!isCacheProcSection
+                        && ((match = matches(CACHE_PROC_START_PATTERN, currLine))) == null) {
+                    // Continue untill the start of cache proc section.
+                    continue;
+                } else {
+                    if (isCacheProcSection) {
+                        // If empty we encountered the end of cached process logging.
+                        if (!currLine.isEmpty()) {
+                            cachedProcessList.add(currLine.trim());
+                        } else {
+                            break;
+                        }
+                    }
+                    isCacheProcSection = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cachedProcessList;
     }
 }
