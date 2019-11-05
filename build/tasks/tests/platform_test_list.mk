3b27f005cab8b45a9ee724e11832b85203870b7c
Daniel Norman <danielnorman@google.com>
2019-11-05 15:39:10 -0800

    RESTRICT AUTOMERGE: Uses TARGET_BOARD_PLATFORM for filtering instead of TARGET_DEVICE.
    Test: n/a
    Change-Id: I42f7fe9a907cde7d75175f702148945e1a8befbe

--- build/tasks/tests/platform_test_list.mk
+++ build/tasks/tests/platform_test_list.mk
@@ -116,7 +116,7 @@ ifneq ($(strip $(BOARD_PERFSETUP_SCRIPT)),)
 platform_tests += perf-setup.sh
 endif
 
-ifneq ($(filter vsoc_x86 vsoc_x86_64, $(TARGET_DEVICE)),)
+ifneq ($(filter vsoc_x86 vsoc_x86_64, $(TARGET_BOARD_PLATFORM)),)
   platform_tests += \
     CuttlefishRilTests \
     CuttlefishWifiTests
