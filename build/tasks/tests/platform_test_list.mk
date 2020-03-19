497ab8c9b93cd730d53245c3c55e58caea48fa64
Joshua Duong <joshuaduong@google.com>
2020-03-19 21:47:00 +0000

    Revert "DO NOT MERGE: Backport building perfetto artifacts."
    Revert submission 10737304-perfetto_backport_configs
    
    Reason for revert: Broken build
    Reverted Changes:
    Id9bb53ecd:DO NOT MERGE: Backport building perfetto artifacts...
    I7eb5c9dc7:DO NOT MERGE: Backport perfetto configs.
    
    Change-Id: I18f3ef81f95b84d038a36efc8029a21a04f8d4d6

--- build/tasks/tests/platform_test_list.mk
+++ build/tasks/tests/platform_test_list.mk
@@ -56,7 +56,6 @@ platform_tests += \
     FrameworksUtilTests \
     InternalLocTestApp \
     JankMicroBenchmarkTests \
-    long_trace_config.textproto \
     LauncherRotationStressTest \
     MemoryUsage \
     MultiDexLegacyTestApp \
@@ -93,9 +92,6 @@ platform_tests += \
     SmokeTestApp \
     SysAppJankTestsWear \
     TouchLatencyJankTestWear \
-    trace_config.textproto \
-    trace_config_detailed.textproto \
-    trace_config_experimental.textproto \
     UbSystemUiJankTests \
     UbWebViewJankTests \
     UiBench \
